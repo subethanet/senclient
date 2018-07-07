c69715e41c43bd79a44f7d9dd2bd9523e021792f
vallery <vallerylancey@gmail.com>
2018-07-07 01:11:53 -0700

    Added Licence (and some stashed work apparently?)

---
+++ crypt/certificateset.go
@@ -0,0 +1,25 @@
+package crypt
+
+import "crypto/x509"
+
+
+type CertificateChain struct {
+	clientCert x509.Certificate
+	identityCert x509.Certificate
+}
+
+
+/*func encryptToCert(unencryptedSecret []byte, targetPubkey *rsa.PublicKey) ([]byte, error) {
+	encryptedSecret, err := rsa.EncryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		targetPubkey,
+		unencryptedSecret,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return encryptedSecret, err
+}*/
